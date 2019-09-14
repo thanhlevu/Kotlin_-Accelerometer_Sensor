0860b5219c357bddba9b2d949556cc4a83a375f2
Thanh Le <nhatthanhh1993@gmail.com>
2019-09-14 12:14:36 +0300

    using accelerometor sensor in Kotlin

---
+++ app/src/main/java/com/example/w2_d4_accelerometer/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.w2_d4_accelerometer
+
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), SensorEventListener {
+
+    lateinit var sensorManager: SensorManager
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        accelerometer_data.text = "x = ${event!!.values[0]}\n\n" +"y = ${event.values[1]}\n\n" + "z = ${event.values[2]}"
+        //accelerometer_data.text = "xxx"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
+        sensorManager.registerListener(
+            this,
+            sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+            SensorManager.SENSOR_DELAY_NORMAL
+        )
+    }
+}
